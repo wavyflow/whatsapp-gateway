13e567696468e18f9ca6e57021cfd62bad62c9f3
Ashary Vermaysha <avermaysha@gmail.com>
2023-05-23 00:16:58 +0700

    feat: add fastify and baileys to backend package dependencies

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(3000, '0.0.0.0');
 
   if (module.hot) {
     module.hot.accept();
