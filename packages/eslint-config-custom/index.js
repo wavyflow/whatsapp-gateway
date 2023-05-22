ca20932d58e4c62581dc234002f760d203486503
Ashary Vermaysha <avermaysha@gmail.com>
2023-05-22 23:44:14 +0700

    feat: add eslint-config-custom package

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+};
