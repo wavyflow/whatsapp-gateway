2d762c055f5b536cccc27d8890caa797fd087031
Ashary Vermaysha <avermaysha@gmail.com>
2023-05-23 12:51:07 +0700

    feat(frontend): add @material-tailwind/react and update eslint config

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,18 +1,25 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+const boxShadow = {
+  sm: "0 0 rgb(0, 0 / 0, 0)",
+  DEFAULT: "0 0 rgb(0, 0 / 0, 0)",
+  md: "0 0 rgb(0, 0 / 0, 0)",
+  lg: "0 0 rgb(0, 0 / 0, 0)",
+  xl: "0 0 rgb(0, 0 / 0, 0)",
+  "2xl": "0 0 rgb(0, 0 / 0, 0)",
+  inner: "0 0 rgb(0, 0 / 0, 0)",
+  none: "0 0 rgb(0, 0 / 0, 0)",
+};
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    boxShadow,
   },
   plugins: [],
-}
+})
